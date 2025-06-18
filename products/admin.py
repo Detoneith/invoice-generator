4dd67967e802cc51e3023387f09a6a2fb09133cc
Stoyan Stoyanov <stoyansho@gmail.com>
2025-06-18 09:53:21 +0200

    Définition du modèle Product, enregistrement de Product dans admin + ajout dans gitignore les migrations et création d'un fichier requirements.txt

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'expiry_date')
+    list_filter = ('expiry_date',)
+    search_fields = ('name',)
